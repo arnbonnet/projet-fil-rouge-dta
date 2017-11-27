3c101b667e22cf114f40de8afa9a5a37c926ec05
Arnaud BONNET <abonnet@DIGINAMIC.LOCAL>
2017-11-27 09:45:06 +0100

    init

---
+++ src/main/java/fr/dta/projetfilrougedta/ProjetFilRougeDtaApplication.java
@@ -0,0 +1,12 @@
+package fr.dta.projetfilrougedta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFilRougeDtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFilRougeDtaApplication.class, args);
+	}
+}
